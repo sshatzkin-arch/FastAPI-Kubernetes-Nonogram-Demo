bac64ef11ea0422d1a17c53fe5fb9b73aa2f3678
Sam Shatzkin <sshatzkin@uchicago.edu>
2021-06-22 12:17:03 -0500

    Added basic python flask and docker setup from tutorials.

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+STUDENTS = {
+  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
+  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
+  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
+  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
+}
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+class StudentsList(Resource):
+  def get(self):
+    return STUDENTS
+
+  parser = reqparse.RequestParser()
+  
+  def post(self):
+    parser.add_argument("name")
+    parser.add_argument("age")
+    parser.add_argument("spec")
+    args = parser.parse_args()
+    student_id = int(max(STUDENTS.keys())) + 1
+    student_id = '%i' % student_id
+    STUDENTS[student_id] = {
+      "name": args["name"],
+      "age": args["age"],
+      "spec": args["spec"],
+    }
+    return STUDENTS[student_id], 201
+
+
+api.add_resource(StudentsList, '/students/')
